78efa7a141974d0adff2474d5d92457c49996623
Makemek <makemek101@hotmail.com>
2016-01-30 02:34:15 +0700

    remove node_modules folder from repo

--- WF/node_modules/bpmn-js/lib/features/replace/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  __depends__: [
-    require('diagram-js/lib/features/popup-menu'),
-    require('diagram-js/lib/features/replace'),
-    require('diagram-js/lib/features/selection')
-  ],
-  bpmnReplace: [ 'type', require('./BpmnReplace') ]
-};
