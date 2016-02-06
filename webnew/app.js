9d014cec8dce6f302a40d87d9c018a90b4695c28
Makemek <makemek101@hotmail.com>
2016-02-06 22:04:54 +0700

    Add error page

--- webnew/app.js
+++ webnew/app.js
@@ -74,7 +74,8 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error_dev', {
+      layout: 'profilePage',
       message: err.message,
       error: err
     });
@@ -86,6 +87,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
+    layout: 'profilePage',
     message: err.message,
     error: {}
   });
