334cf100c2e949bc0a60d1bcf0fd605d81358610
makemek <makemek@users.noreply.github.com>
2016-02-15 19:09:20 +0700

    Revert "Connect db index page"

--- webnew/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var db =  require('./lib/dmsDb');
-var app = require('./app')(db);
-
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
-});
